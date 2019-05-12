e7bb62f679a6fe3397e60b608901f90ec7c0a071
Georgy Gerbst <gerbst.g@gmail.com>
2019-05-12 21:45:50 +0300

    Переключение валюты (#task3)

--- src/components/currency-filter/currency-filter.js
+++ src/components/currency-filter/currency-filter.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Currencies from '../currencies';
+import withTicketsService from '../hoc/with-tickets-service';
+import { compose } from "../../utils";
+import { filterByCurrency } from '../../actions';
+import { createSelector } from 'reselect';
 
 import './index.scss';
 
-const Filter = ({className}) => {
+const Filter = (props) => {
+
+	const { className, currency, onCurrencyChange } = props;
+
 	return(
 		<div className={`${className} filter`}>
 			<div className="filter__header">Валюта</div>
 			<div className="filter__content">
-				<Currencies />
+				<Currencies currency={currency} onCurrencyChange={onCurrencyChange} />
 			</div>
 		</div>
 	)
 };
 
-export default Filter;
+// кажется, диспатч тут не нужен. нужно в селекторе написать то что сейчас у меня в функции которая над редюсером.
+
+const mapStateToProps = (state) => {
+	return {
+		currency: state.currency
+	}
+};
+
+// вместо того чтобы писать mapStateToProps можно уже в самомконнекте написать объект,
+// где ключом будет метод а значением функция которая диспачится. обычно аргументы одни и теже поэтому они не нужны
+export default compose(withTicketsService(), connect(mapStateToProps, { onCurrencyChange: filterByCurrency }))(Filter);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,33 +1,69 @@
-const reducer = (state, action) => {
-	if (state === undefined) {
-		return {
-			tickets: [],
-			loading: true,
-			error: null
+let initialState = {
+	initialTickets: [],
+	tickets: [],
+	loading: true,
+	error: null,
+	currency: 'RUB'
+};
+
+const updateTicketsList = (state, curr) => {
+
+	const { initialTickets } = state;
+
+	const newCurrency = initialTickets.map((ticket) => {
+		if (curr === 'RUB') {
+			return ticket
+		} else if (curr === 'EUR') {
+			return {
+				...ticket,
+				price: (ticket.price/73).toFixed(0)
+			}
+		} else if (curr === 'USD') {
+			return {
+				...ticket,
+				price: (ticket.price/65).toFixed(0)
+			}
 		}
+	});
+
+	return {
+		...state,
+		tickets: newCurrency,
+		currency: curr
 	}
+};
+
+const reducer = (state = initialState, action) => {
 
 	switch (action.type) {
 		case 'FETCH_TICKETS_REQUEST':
 			return {
 				tickets: [], // либо spread ...state
+				initialTickets: [],
 				loading: true,
-				error: null
+				error: null,
+				currency: 'RUB'
 			};
 		case 'FETCH_TICKETS_SUCCESS':
 			return {
 				tickets: action.payload,
+				initialTickets: action.payload,
 				loading: false,
-				error: null
+				error: null,
+				currency: 'RUB'
 			};
 		case 'FETCH_TICKETS_FAILURE':
 			return {
 				tickets: [],
+				initialTickets: [],
 				loading: false,
-				error: action.payload
+				error: action.payload,
+				currency: 'RUB'
 			};
+		case 'FILTER_BY_CURRENCY':
+			return updateTicketsList(state, action.payload);
 		default:
-			return state.tickets
+			return state
 	}
 };
 
